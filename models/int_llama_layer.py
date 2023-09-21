e023d4a5793778872a69159715510f3d090f9820
Fuwen Tan <fuwen.tan@gmail.com>
2023-09-21 19:24:24 +0000

    llama

--- models/int_llama_layer.py
+++ models/int_llama_layer.py
@@ -1,45 +1,31 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
+
+import math, copy
+from collections import OrderedDict
 from typing import Optional, Tuple, List
+
+from models.transformation import *
 from quantize.int_linear import QuantLinear
 from quantize.int_matmul import QuantMatMul
-import torch.nn.functional as F
 from quantize.omni_norm import OmniLlamaRMSNorm
-from collections import OrderedDict
-import math
+
 from transformers.models.llama.modeling_llama import LlamaRotaryEmbedding,apply_rotary_pos_emb,LlamaRMSNorm,repeat_kv
 from transformers.models.llama.configuration_llama import LlamaConfig
 from transformers.activations import ACT2FN
-import pdb
-import copy
-from models.transformation import *
-
-
 
 
 class QuantLlamaMLP(nn.Module):
-    def __init__(
-        self,
-        org_module: nn.Module,
-        hidden_size: int,
-        intermediate_size: int,
-        hidden_act: str,
-        args=None,
-    ):
+    def __init__(self, org_module: nn.Module, hidden_size: int, intermediate_size: int, hidden_act: str, args=None):
         super().__init__()
         # self.gate_proj = nn.Linear(hidden_size, intermediate_size, bias=False)
         # self.down_proj = nn.Linear(intermediate_size, hidden_size, bias=False)
         # self.up_proj = nn.Linear(hidden_size, intermediate_size, bias=False)
-        self.gate_proj = QuantLinear(org_module.gate_proj,
-                                           args.weight_quant_params,
-                                           args.act_quant_params)
-        self.down_proj = QuantLinear(org_module.down_proj,
-                                           args.weight_quant_params,
-                                           args.act_quant_params)
-        self.up_proj = QuantLinear(org_module.up_proj,
-                                           args.weight_quant_params,
-                                           args.act_quant_params)
-        self.act_fn = ACT2FN[hidden_act]
+        self.gate_proj = QuantLinear(org_module.gate_proj, args.weight_quant_params, args.act_quant_params)
+        self.down_proj = QuantLinear(org_module.down_proj, args.weight_quant_params, args.act_quant_params)
+        self.up_proj   = QuantLinear(org_module.up_proj,   args.weight_quant_params, args.act_quant_params)
+        self.act_fn    = ACT2FN[hidden_act]
 
     def forward(self, x):
         return self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
@@ -48,10 +34,7 @@ class QuantLlamaMLP(nn.Module):
 class QuantLlamaAttention(nn.Module):
     """Multi-headed attention from 'Attention Is All You Need' paper"""
 
-    def __init__(self, 
-                 org_module: nn.Module,
-                 config: LlamaConfig,
-                 args=None):
+    def __init__(self, org_module: nn.Module, config: LlamaConfig, args=None):
         super().__init__()
         self.config = config
         self.hidden_size = config.hidden_size
@@ -68,54 +51,26 @@ class QuantLlamaAttention(nn.Module):
             )
 
         self.rotary_emb = copy.deepcopy(org_module.rotary_emb)
-
-        self.k_proj = QuantLinear(
-            org_module.k_proj,
-            args.weight_quant_params,
-            args.act_quant_params,
-        )
-        self.v_proj = QuantLinear(
-            org_module.v_proj,
-            args.weight_quant_params,
-            args.act_quant_params,
-        )
-        self.q_proj = QuantLinear(
-            org_module.q_proj,
-            args.weight_quant_params,
-            args.act_quant_params,
-        )
-        self.o_proj = QuantLinear(
-            org_module.o_proj, args.weight_quant_params, args.act_quant_params
-        )
-        self.qkt_matmul = QuantMatMul(
-            args.q_quant_params, args.k_quant_params, matmul_func=torch.matmul
-        )
-        self.pv_matmul = QuantMatMul(
-            args.p_quant_params, args.v_quant_params, matmul_func=torch.matmul
-        )
-
+        self.k_proj = QuantLinear(org_module.k_proj, args.weight_quant_params, args.act_quant_params)
+        self.v_proj = QuantLinear(org_module.v_proj, args.weight_quant_params, args.act_quant_params)
+        self.q_proj = QuantLinear(org_module.q_proj, args.weight_quant_params, args.act_quant_params)
+        self.o_proj = QuantLinear(org_module.o_proj, args.weight_quant_params, args.act_quant_params)
+        self.qkt_matmul = QuantMatMul(args.q_quant_params, args.k_quant_params, matmul_func=torch.matmul)
+        self.pv_matmul = QuantMatMul(args.p_quant_params, args.v_quant_params, matmul_func=torch.matmul)
         self.use_weight_quant = False
         self.use_act_quant = False
 
     def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
         return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
 
-    def forward(
-        self,
-        hidden_states: torch.Tensor,
-        attention_mask: Optional[torch.Tensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        past_key_value: Optional[Tuple[torch.Tensor]] = None,
-        output_attentions: bool = False,
-        use_cache: bool = False,
-    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+    def forward(self, hidden_states: torch.Tensor, attention_mask: Optional[torch.Tensor] = None, position_ids: Optional[torch.LongTensor] = None, past_key_value: Optional[Tuple[torch.Tensor]] = None, output_attentions: bool = False, use_cache: bool = False) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
         bsz, q_len, _ = hidden_states.size()
 
         # query_states = self.q_proj(hidden_states)
         # key_states = self.k_proj(hidden_states)
         # value_states = self.v_proj(hidden_states)
-        query_states = self.q_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-        key_states =self.k_proj(hidden_states).view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        query_states = self.q_proj(hidden_states).view(bsz, q_len, self.num_heads,           self.head_dim).transpose(1, 2)
+        key_states   = self.k_proj(hidden_states).view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
         value_states = self.v_proj(hidden_states).view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
 
         kv_seq_len = key_states.shape[-2]
@@ -139,7 +94,7 @@ class QuantLlamaAttention(nn.Module):
         value_states = repeat_kv(value_states, self.num_key_value_groups)
         
         query_states = self.qkt_matmul.quant_x1(query_states)
-        key_states = self.qkt_matmul.quant_x2(key_states)
+        key_states   = self.qkt_matmul.quant_x2(key_states)
         attn_weights = self.qkt_matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
 
         if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
@@ -187,28 +142,14 @@ class QuantLlamaAttention(nn.Module):
                 m.set_quant_state(weight_quant, act_quant)
                 
 
-
 class QuantLlamaDecoderLayer(nn.Module):
-    def __init__(self, 
-                 config: LlamaConfig,
-                 ori_layer,
-                 args):
+    def __init__(self, config: LlamaConfig, ori_layer, args):
         super().__init__()
         self.hidden_size = config.hidden_size
-        self.self_attn = QuantLlamaAttention(
-            org_module=ori_layer.self_attn,
-            config=config,
-            args=args,
-            )
-        self.mlp = QuantLlamaMLP(
-            org_module=ori_layer.mlp,
-            hidden_size=self.hidden_size,
-            intermediate_size=config.intermediate_size,
-            hidden_act=config.hidden_act,
-            args=args,
-        )
-        self.input_layernorm = OmniLlamaRMSNorm(ori_layer.input_layernorm,eps=ori_layer.input_layernorm.variance_epsilon)
-        self.post_attention_layernorm = OmniLlamaRMSNorm(ori_layer.post_attention_layernorm,eps=ori_layer.post_attention_layernorm.variance_epsilon)
+        self.self_attn = QuantLlamaAttention(org_module=ori_layer.self_attn, config=config, args=args)
+        self.mlp = QuantLlamaMLP(org_module=ori_layer.mlp, hidden_size=self.hidden_size, intermediate_size=config.intermediate_size, hidden_act=config.hidden_act, args=args)
+        self.input_layernorm = OmniLlamaRMSNorm(ori_layer.input_layernorm, eps=ori_layer.input_layernorm.variance_epsilon)
+        self.post_attention_layernorm = OmniLlamaRMSNorm(ori_layer.post_attention_layernorm, eps=ori_layer.post_attention_layernorm.variance_epsilon)
 
     def forward(
         self,
@@ -233,9 +174,7 @@ class QuantLlamaDecoderLayer(nn.Module):
             past_key_value (`Tuple(torch.FloatTensor)`, *optional*): cached past key and value projection states
         """
         residual = hidden_states
-
         hidden_states = self.input_layernorm(hidden_states)
-
 
         # Self Attention
         hidden_states, self_attn_weights, present_key_value = self.self_attn(
@@ -252,7 +191,6 @@ class QuantLlamaDecoderLayer(nn.Module):
         residual = hidden_states
         hidden_states = self.post_attention_layernorm(hidden_states)
         
-
         hidden_states = self.mlp(hidden_states)
         hidden_states = residual + hidden_states
 
@@ -282,14 +220,11 @@ class QuantLlamaDecoderLayer(nn.Module):
                 for name, module in self.named_parameters():
                     if "smooth_scale" in name:
                         module.data = truncate_number(module)
-            smooth_ln_fcs_temporary(self.input_layernorm,[self.self_attn.q_proj, self.self_attn.k_proj, self.self_attn.v_proj],
-                                    self.qkv_smooth_scale,self.qkv_smooth_shift)
-            smooth_ln_fcs_temporary(self.post_attention_layernorm,[self.mlp.up_proj,self.mlp.gate_proj],
-                                    self.fc1_smooth_scale,self.fc1_smooth_shift)
-            smooth_fc_fc_temporary(self.self_attn.v_proj,self.self_attn.o_proj,
-                                self.out_smooth_scale, self.out_smooth_shift)
-            smooth_q_k_temporary(self.self_attn.q_proj, self.self_attn.k_proj,
-                                self.qkt_smooth_scale)
+            smooth_ln_fcs_temporary(self.input_layernorm, [self.self_attn.q_proj, self.self_attn.k_proj, self.self_attn.v_proj], self.qkv_smooth_scale, self.qkv_smooth_shift)
+            smooth_ln_fcs_temporary(self.post_attention_layernorm,[self.mlp.up_proj,self.mlp.gate_proj], self.fc1_smooth_scale, self.fc1_smooth_shift)
+            smooth_fc_fc_temporary(self.self_attn.v_proj,self.self_attn.o_proj, self.out_smooth_scale, self.out_smooth_shift)
+            smooth_q_k_temporary(self.self_attn.q_proj, self.self_attn.k_proj, self.qkt_smooth_scale)
+            # not quantizing down_proj?
             self.mlp.down_proj.temp_weight = self.mlp.down_proj.weight
         else:
             for name, module in self.named_modules():
@@ -318,14 +253,10 @@ class QuantLlamaDecoderLayer(nn.Module):
             for name, module in self.named_parameters():
                 if "smooth_scale" in name:
                     module.data = truncate_number(module)
-            smooth_ln_fcs_inplace(self.input_layernorm,[self.self_attn.q_proj, self.self_attn.k_proj, self.self_attn.v_proj],
-                                    self.qkv_smooth_scale,self.qkv_smooth_shift)
-            smooth_ln_fcs_inplace(self.post_attention_layernorm,[self.mlp.up_proj,self.mlp.gate_proj],
-                                    self.fc1_smooth_scale,self.fc1_smooth_shift)
-            smooth_fc_fc_inplace(self.self_attn.v_proj,self.self_attn.o_proj,
-                                self.out_smooth_scale, self.out_smooth_shift)
-            smooth_q_k_inplace(self.self_attn.q_proj, self.self_attn.k_proj,
-                                self.qkt_smooth_scale)
+            smooth_ln_fcs_inplace(self.input_layernorm, [self.self_attn.q_proj, self.self_attn.k_proj, self.self_attn.v_proj], self.qkv_smooth_scale, self.qkv_smooth_shift)
+            smooth_ln_fcs_inplace(self.post_attention_layernorm, [self.mlp.up_proj, self.mlp.gate_proj], self.fc1_smooth_scale, self.fc1_smooth_shift)
+            smooth_fc_fc_inplace(self.self_attn.v_proj, self.self_attn.o_proj, self.out_smooth_scale, self.out_smooth_shift)
+            smooth_q_k_inplace(self.self_attn.q_proj, self.self_attn.k_proj, self.qkt_smooth_scale)
         for name, module in self.named_modules():
             if isinstance(module, QuantLinear):
                 module.weight = module.weight_quantizer(module.weight)
